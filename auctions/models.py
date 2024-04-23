16a7ecdce2e11145fc8d605748c48886fd4fe529
shumit taher <shumitt@gmail.com>
2024-04-23 21:38:11 +0600

    create Listing page started

--- auctions/models.py
+++ auctions/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class User(AbstractUser):
     phone_number = models.CharField(max_length = 11)
     pass
+
+
+class Listings(models.Model):
+    object_id = models.AutoField(primary_key=True)
+    object_name = models.CharField(max_length=64)
+    object_price = models.DecimalField(decimal_places=2, max_digits=8)
+    object_description = models.TextField
+    object_picture = models.FileField(upload_to="uploads/")
