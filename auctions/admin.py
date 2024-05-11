1cddc744b78c4ca3a2871d84c13ee4d1cd615a84
shumit <shumitt@gmail.com>
2024-05-11 07:35:04 -0700

    minor change in admin module

--- auctions/admin.py
+++ auctions/admin.py
@@ -7,9 +7,12 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ('object_name', 'object_price', 'category', 'object_lister')
 class BidsAdmin(admin.ModelAdmin):
     list_display = ( 'object_id', 'bid_amount', 'bidder_id')
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ( 'object_id', 'comment_text', 'commenter_id')
 
 
 admin.site.register(User)
+admin.site.register(Comments, CommentsAdmin)
 admin.site.register(Listings, ListingAdmin)
 admin.site.register(Categories)
 admin.site.register(Bids, BidsAdmin)
